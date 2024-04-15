fece6e405aeffc30d2fca85b0e3fe6e71113d78b
sanwuu <san7code@gmail.com>
2024-04-15 07:30:29 +0530

    n queen prob

---
+++ Backtraking/n-quuen.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isSafe(int row, int col, vector<vector<int> > &grid, int n){
+    //for row we know x value will be constant and column will vary right  to left so no need to check for rows
+    int x =row;
+    int y =col;
+    while(/*check the condtion till when it will check yaha pe mtlb ye last wale tak pahcuh jayega to chor dena mtlbl kya col>=0*/){
+     
+    }
+    // yaha pe check karne ka ek aur tarika hai aur that is to check using for loop for(col-1)
+}  
+
+int  main(){
+
+}
