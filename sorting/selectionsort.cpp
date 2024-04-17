5db79362c42c299db99635585532b3f5cbb49789
sonuarman <mansooriarman090@gmail.com>
2024-04-18 02:34:03 +0530

    selection

---
+++ sorting/selectionsort.cpp
@@ -0,0 +1,18 @@
+//template
+#include<iostream>
+using namespace std;
+void func(vector<int>arr, int n){
+    
+}
+int main(){
+    vector<int>arr;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int n=arr.size();
+    func(arr, n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+    return 0;
+}
