61c8f2a1dd3e868847cd20186422b40706e11fdf
sonuarman <mansooriarman090@gmail.com>
2024-04-26 11:17:14 +0530

    copiler correct

--- sorting/Cp/arena.cpp
+++ sorting/Cp/arena.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 typedef long long ll;
 
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -16,17 +15,16 @@ int main() {
         ll n; 
         cin >> n;
         vector<ll> v(n);
-        int min =INT_MAX;
+        int min_val = INT_MAX; // Changed variable name to avoid conflict with 'std::min'
         for (ll i = 0; i < n; i++) { 
             cin >> v[i];
-            min=min(min, v[i]);
-
+            min_val = min(min_val, v[i]); // Used min_val instead of min
         }
-        ll xt;
-        for(auto i : v){
-            if(i == min) xt++;
+        ll xt = 0; // Initialized xt to 0
+        for (auto i : v) {
+            if (i == min_val) xt++;
         }
-        cout<<n-xt;
+        cout << n - xt;
     }
 
     return 0;
